6d53faae32bea1dd5c0ce32a4ac09dfcc4c46f0b
QTpierogi <76211845+QTpierogi@users.noreply.github.com>
2021-05-25 01:52:41 +0500

    Demo level with new camera and invisible borders

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     public Transform target;
-    public float yBias = 1.9f;
+    public float yBias = 3.5f;
+    public float updateSpeed = 3;
+    public float xBorder;
+    public float yBorder;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new Vector3(0, yBias, -10);
+        Vector3 startPosition= new Vector3(Mathf.Clamp(target.position.x, -xBorder, xBorder), Mathf.Clamp(target.position.y, -yBorder, yBorder), 0) + offset;
+        transform.position = startPosition;
+    }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y + yBias, -10);
+        //transform.position = new Vector3(target.position.x, target.position.y + yBias, -10);
+        Vector3 positionToMove = new Vector3(Mathf.Clamp(target.position.x, -xBorder, xBorder), Mathf.Clamp(target.position.y, -yBorder, yBorder), 0) + offset;
+        var newPos = Vector3.MoveTowards(transform.position, positionToMove, updateSpeed * Time.deltaTime);
+        transform.position = newPos;
     }
 }
